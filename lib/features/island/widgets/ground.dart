1bfc06968b4af2b6d5337716bb2c17a99246a70d
lucas-goldner <lucas.goldner@googlemail.com>
2024-06-21 20:25:22 +0200

    feat: Add a bit of overlay between ground textures

--- lib/features/island/widgets/ground.dart
+++ lib/features/island/widgets/ground.dart
@@ -1,24 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:pushup_bro/core/extensions/build_context_ext.dart';
 
 class Ground extends StatelessWidget {
   const Ground({super.key});
 
   @override
-  Widget build(BuildContext context) => Column(
+  Widget build(BuildContext context) => Stack(
         children: [
-          Flexible(
-            flex: 4,
-            child: ColoredBox(
-              color: context.gameColorsTheme.grass,
-              child: const SizedBox.expand(),
-            ),
+          Column(
+            children: [
+              Flexible(
+                flex: 4,
+                child: ColoredBox(
+                  color: context.gameColorsTheme.grass,
+                  child: const SizedBox.expand(),
+                ),
+              ),
+              Flexible(
+                flex: 5,
+                child: ColoredBox(
+                  color: context.gameColorsTheme.sand,
+                  child: const SizedBox.expand(),
+                ),
+              ),
+            ],
           ),
-          Flexible(
-            flex: 5,
-            child: ColoredBox(
-              color: context.gameColorsTheme.sand,
-              child: const SizedBox.expand(),
+          Positioned(
+            top: context.sizeOf.height / 2.5,
+            left: -20,
+            child: Row(
+              children: [
+                Container(
+                  width: 100,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: context.gameColorsTheme.sand,
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+                const SizedBox(
+                  width: 100,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 32),
+                  child: Container(
+                    width: 100,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      color: context.gameColorsTheme.sand,
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
